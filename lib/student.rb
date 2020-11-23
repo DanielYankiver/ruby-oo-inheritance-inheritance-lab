94b4f3a549a1f2c79708f19278a43fa6dbedd22d
Daniel <dyankiver@gmail.com>
2020-11-23 08:32:00 -0500

    initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+      @knowledge = []
+    end
+
+    #Student #learn takes in an argument 
+    # of a string of knowledge and adds it 
+    # to the student's knowledge array
+  
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+
+    #Student #knowledge returns that 
+    # student's knowledge array
+  
+    def knowledge
+      @knowledge
+    end
 
 end
